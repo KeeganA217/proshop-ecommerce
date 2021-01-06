c2e9fedafee8acabfb05dc62e10d40051b3a08d5
Keegan <keegan.adams15@gmail.com>
2021-01-05 19:28:42 -0600

    bulding product components fro display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import { Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <Fragment>
-      <h1>Hello world</h1>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </Fragment>
+    </Router>
   );
 };
 
