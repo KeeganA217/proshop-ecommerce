5e2f8cb2ac4351f79a98d7dc723aeff57d8ba00f
Keegan <keegan.adams15@gmail.com>
2021-01-13 19:41:49 -0600

    completed cart functions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,13 +2,16 @@ import {
   PRODUCTS_LIST_REQUEST,
   PRODUCTS_LIST_SUCCESS,
   PRODUCTS_LIST_FAIL,
+  PRODUCTS_DETAILS_REQUEST,
+  PRODUCTS_DETAILS_SUCCESS,
+  PRODUCTS_DETAILS_FAIL,
 } from "../constants/productConstants";
 import axios from "axios";
 
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({
-      typ: PRODUCTS_LIST_REQUEST,
+      type: PRODUCTS_LIST_REQUEST,
     });
 
     const { data } = await axios.get("/api/products");
@@ -20,6 +23,28 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCTS_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCTS_DETAILS_REQUEST,
+    });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCTS_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCTS_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
