9638fddadd092baaf843b41ea2bc015eb17b20c1
Keegan <keegan.adams15@gmail.com>
2021-01-25 19:10:44 -0600

    created orders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,8 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 const App = () => {
   return (
@@ -19,7 +21,9 @@ const App = () => {
         <main className="py-3">
           <Container>
             <Route path="/login" component={LoginScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
             <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
             <Route path="/profile" component={ProfileScreen} />
             <Route path="/register" component={RegisterScreen} />
             <Route path="/product/:id" component={ProductScreen} />
